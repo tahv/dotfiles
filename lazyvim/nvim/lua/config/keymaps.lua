c98eece1a68d99e84653f0f7a66873fef249faed
Thibaud Gambier <thibaud.gambier@gmail.com>
2023-05-30 22:16:14 +0200

    nvim override C-u and C-d keymaps

--- lazyvim/nvim/lua/config/keymaps.lua
+++ lazyvim/nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Better view  scroll
+vim.keymap.set(
+  "n",
+  "<C-u>",
+  "<C-u>zz",
+  {desc = "Scroll window upward and center the view"}
+)
+
+vim.keymap.set(
+  "n",
+  "<C-d>",
+  "<C-d>zz",
+  {desc = "Scroll window downward and center the view"}
+)
