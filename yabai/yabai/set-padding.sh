bb49e6a3fd1ea66bf07834bacb63a8be3e374ee2
Thibaud Gambier <thibaud.gambier@gmail.com>
2023-10-01 11:40:41 +0200

    fixed float comparison

--- yabai/yabai/set-padding.sh
+++ yabai/yabai/set-padding.sh
@@ -10,18 +10,19 @@ for row in $visible_spaces; do
   display=$(echo $row | jq '.display')
   display_width=$(yabai -m query --displays --display $display | jq '.frame.w')
   num_windows=$(yabai -m query --windows --display $display | jq '[.[] | select(."is-visible"==true and ."is-floating"==false)] | length')
-
-  # On a widescreen, add padding to the sides if there is only one window
-  if [ $display_width -ge 3440 ] && [ $num_windows -eq 1 ]
+  # echo space: $space, display: $display, width: $display_width, num_wins: $num_windows
+  
+  # On a widescreen, add padding to the sides if there is only one window.
+  # Using bc (basic calculator). Bash does not support floating point arithmetic.
+  # https://stackoverflow.com/a/31087503
+  if (( $(echo "$display_width >= 3440" | bc -l) )) && [ $num_windows -eq 1 ]
   then
     # widescreen with only one managed window
     yabai -m space $space --padding abs:10:10:600:600
     # yabai -m space $space --gap abs:10
-    # echo $space $display $display_width $space widescreen one win
   else
     # native screen or widescreen with multiple managed windowss
     yabai -m space $space --padding abs:10:10:10:10
     # yabai -m space $space --gap abs:10
-    # echo $space $display $display_width $space base case
   fi
 done
