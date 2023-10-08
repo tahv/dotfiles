0193f8bf6a0658ae663aeab103e2bc7f20cd263c
Thibaud Gambier <thibaud.gambier@gmail.com>
2023-10-08 15:09:44 +0200

    cleanup bufremove

--- neovim/nvim/lua/plugins/mini-bufremove.lua
+++ neovim/nvim/lua/plugins/mini-bufremove.lua
@@ -3,8 +3,7 @@ return {
   'echasnovski/mini.bufremove',
   version = '*', -- Stable version
   config = function()
-    local bufremove = require('mini.bufremove')
-    vim.keymap.set('n', '<leader>bd', function() bufremove.delete(0, false) end, { desc = '[B]uffer [D]elete' })
-    vim.keymap.set('n', '<leader>bD', function() bufremove.delete(0, true) end, { desc = '[B]uffer [D]elete (force)' })
+    vim.keymap.set('n', '<leader>bd', function() require('mini.bufremove').delete(0, false) end, { desc = '[B]uffer [D]elete' })
+    vim.keymap.set('n', '<leader>bD', function() require('mini.bufremove').delete(0, true) end, { desc = '[B]uffer [D]elete (Force)' })
   end,
 }
