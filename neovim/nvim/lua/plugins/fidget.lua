6f0741f06eec990426f8a3461e343b427c2181ab
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-02-14 21:27:27 +0100

    update fidget to stable

--- neovim/nvim/lua/plugins/fidget.lua
+++ neovim/nvim/lua/plugins/fidget.lua
@@ -1,10 +1,6 @@
--- Standalone UI for nvim-lsp progress 
+-- Extensible UI for Neovim notifications and LSP progress messages. 
 return {
   'j-hui/fidget.nvim',
-  -- fidget.nvim will soon be completely rewritten.
-  -- Pinning configuration to the legacy branch to avoid breaking changes.
-  -- https://github.com/j-hui/fidget.nvim/issues/131
-  tag = 'legacy',
   config = function()
     require('fidget').setup({})
   end,
