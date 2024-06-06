096a179afee890b7f2964948946d08433d2437ea
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-06-06 23:17:08 +0200

    fix: highlight on yank

--- neovim/nvim/lua/config/autocmds.lua
+++ neovim/nvim/lua/config/autocmds.lua
@@ -1,9 +1,11 @@
 vim.api.nvim_create_autocmd(
   "TextYankPost",
   {
-    callback = vim.highlight.on_yank,
+    desc = "Highlight on yank",
+    callback = function()
+      vim.highlight.on_yank()
+    end,
     group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
     pattern = "*",
-    desc = "Highlight on yank",
   }
 )
