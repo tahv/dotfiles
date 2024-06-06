a11fa1f6e52710ca42fdece7d1e0869eaafa2cfe
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-06-06 18:09:12 +0200

    changed diagflow formatting

--- neovim/nvim/lua/plugins/diagflow-nvim.lua
+++ neovim/nvim/lua/plugins/diagflow-nvim.lua
@@ -1,9 +1,15 @@
 -- LSP diagnostics in virtual text at the top right of your screen
 return {
-    'dgagn/diagflow.nvim',
-    event = 'LspAttach',
+    "dgagn/diagflow.nvim",
+    event = "LspAttach",
     opts = {
-        scope = 'line',
+        scope = "line",
         padding_right = 1,
-    }
+        format = function(diag)
+            if diag.code == nil then
+                return diag.message
+            end
+            return string.format("[%s] %s", diag.code, diag.message)
+        end,
+    },
 }
