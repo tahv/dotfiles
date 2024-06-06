d8fd4c782530b27ea7c3cfddc9b772b9f6718c0c
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-06-06 22:43:37 +0200

    refactor: navigator keys using lazyvim api

--- neovim/nvim/lua/plugins/navigator-nvim.lua
+++ neovim/nvim/lua/plugins/navigator-nvim.lua
@@ -1,13 +1,14 @@
 -- Smoothly navigate between neovim and terminal multiplexer(s)
 return {
   "numToStr/Navigator.nvim",
-  config = function()
-    require('Navigator').setup()
-
-    -- Navigate windows using <ctrl> hjkl keys
-    vim.keymap.set('n', '<C-h>', '<CMD>NavigatorLeft<CR>', { desc = 'Go to left window' })
-    vim.keymap.set('n', '<C-j>', '<CMD>NavigatorDown<CR>', { desc = 'Go to lower window' })
-    vim.keymap.set('n', '<C-k>', '<CMD>NavigatorUp<CR>', { desc = 'Go to upper window' })
-    vim.keymap.set('n', '<C-l>', '<CMD>NavigatorRight<CR>', { desc = 'Go to right window' })
+  config = function(_, opts)
+    require "Navigator".setup(opts)
   end,
+  keys = {
+    -- Navigate windows using <ctrl> hjkl keys
+    { "<C-h>", "<CMD>NavigatorLeft<CR>",  desc = "Go to left window" },
+    { "<C-j>", "<CMD>NavigatorDown<CR>",  desc = "Go to lower window" },
+    { "<C-k>", "<CMD>NavigatorUp<CR>",    desc = "Go to upper window" },
+    { "<C-l>", "<CMD>NavigatorRight<CR>", desc = "Go to right window" },
+  },
 }
