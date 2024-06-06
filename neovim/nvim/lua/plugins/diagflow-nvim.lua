b3d0990f0f4a9e3eb0d7ea0f69f2f0890fbc710b
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-06-06 23:12:34 +0200

    refactor: diagflow format_diagnostic

--- neovim/nvim/lua/plugins/diagflow-nvim.lua
+++ neovim/nvim/lua/plugins/diagflow-nvim.lua
@@ -1,15 +1,27 @@
 -- LSP diagnostics in virtual text at the top right of your screen
+
+---@param diagnostic vim.Diagnostic
+---@return string
+local function format_diagnostic(diagnostic)
+    local text = ""
+
+    if diagnostic.code then
+        text = text .. string.format("[%s] ", diagnostic.code)
+    end
+
+    if diagnostic.source then
+        text = text .. string.format("%s: ", diagnostic.source)
+    end
+
+    return text .. diagnostic.message
+end
+
 return {
     "dgagn/diagflow.nvim",
     event = "LspAttach",
     opts = {
         scope = "line",
         padding_right = 1,
-        format = function(diagnostic)
-            if diagnostic.code == nil then
-                return diagnostic.message
-            end
-            return string.format("[%s] %s", diagnostic.code, diagnostic.message)
-        end,
+        format = format_diagnostic,
     },
 }
