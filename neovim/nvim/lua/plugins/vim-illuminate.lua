991ca945b6cd01bf2aaf2c7e7bc3099650496ff3
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-06-06 22:43:15 +0200

    chore: illuminate cutoff large files

--- neovim/nvim/lua/plugins/vim-illuminate.lua
+++ neovim/nvim/lua/plugins/vim-illuminate.lua
@@ -1,5 +1,15 @@
--- Highlighting other uses of the word under the cursor using either LSP, Tree-sitter, or regex matching. 
+-- Automatically highlighting other uses of the word under the cursor
+-- using either LSP, Tree-sitter, or regex matching.
 return {
   "RRethy/vim-illuminate",
-  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+  opts = {
+    large_file_cutoff = 2000, -- number of lines at which to use large_file_overrides
+    large_file_overrides = {
+      providers = { "lsp" },
+    },
+  },
+  config = function(_, opts)
+    require("illuminate").configure(opts)
+  end
 }
