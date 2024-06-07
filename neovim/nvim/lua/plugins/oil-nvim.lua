430c3e15bffe4635bdaff0346411ae79c97f1402
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-06-07 21:23:15 +0200

    add: oil open root; formatting

--- neovim/nvim/lua/plugins/oil-nvim.lua
+++ neovim/nvim/lua/plugins/oil-nvim.lua
@@ -1,22 +1,32 @@
 return {
-  'stevearc/oil.nvim',
-  -- opts = {},
+  "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      view_options = {
-        show_hidden = true,
-      },
-      skip_confirm_for_simple_edits = true,
-      keymaps = {
-        ["<C-s>"] = false,
-        ["<C-h>"] = false,
-        ["<C-l>"] = false,
-        ["<C-r>"] = "actions.refresh",
-      },
-    })
-
-    vim.keymap.set("n", "<leader>e", require("oil").open, { desc = "Open file [E]xplorer" })
-    -- vim.keymap.set("n", "<leader>e", require("oil").toggle_float, { desc = "Toggle file [E]xplorer" })
-  end,
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    skip_confirm_for_simple_edits = true,
+    keymaps = {
+      ["<C-s>"] = false,
+      ["<C-h>"] = false,
+      ["<C-l>"] = false,
+      ["<C-r>"] = "actions.refresh",
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("oil").open()
+      end,
+      desc = "[E]xplorer (Buffer Dir)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("oil").open(vim.uv.cwd())
+      end,
+      desc = "[E]xplorer (Root)",
+    },
+  },
 }
