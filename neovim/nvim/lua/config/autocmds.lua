a56c653cd923b3d843280fe886cb370a776cc3da
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-06-11 23:02:54 +0200

    refactor: autocmds

--- neovim/nvim/lua/config/autocmds.lua
+++ neovim/nvim/lua/config/autocmds.lua
@@ -1,11 +1,7 @@
-vim.api.nvim_create_autocmd(
-  "TextYankPost",
-  {
-    desc = "Highlight on yank",
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-    pattern = "*",
-  }
-)
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight on yank",
+  group = vim.api.nvim_create_augroup("tahv-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
