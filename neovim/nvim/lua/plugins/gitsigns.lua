2d7f266ffc1f4e0af1a9f5adf471685460535a38
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-09-24 13:33:47 +0200

    updated gitsigns config

--- neovim/nvim/lua/plugins/gitsigns.lua
+++ neovim/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,3 @@
-
 local function previous_hunk()
   if vim.wo.diff then
     vim.cmd.normal({ "[c", bang = true })
@@ -15,30 +14,53 @@ local function next_hunk()
   end
 end
 
-return {
-  -- Git integration for buffers.
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    signs = {
-      add = { text = "┃" },
-      change = { text = "┃" },
-      delete = { text = "▁" },
-      topdelete = { text = "▔" },
-      changedelete = { text = "~" },
-      untracked = { text = "┆" },
-    },
-    current_line_blame_formatter = "<author_time:%d/%m/%y %H:%MS> <author> - <summary>",
-    current_line_blame_opts = {
-      delay = 100,
-    },
-    on_attach = function(bufnr)
-      local gitsigns = require("gitsigns")
+local function stage_visual_hunk()
+  require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+end
 
-      vim.keymap.set("n", "]c", next_hunk, { buffer = bufnr, desc = "Next hunk" })
-      vim.keymap.set("n", "[c", previous_hunk, { buffer = bufnr, desc = "Previous hunk" })
-      vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame, { buffer = bufnr, desc = "[T]oggle line [B]lame" })
-      vim.keymap.set("n", "<leader>td", gitsigns.toggle_deleted, { buffer = bufnr, desc = "[T]oggle git show [D]eleted" })
-    end,
+---@type LazySpec[]
+return {
+  {
+    -- Git integration for buffers.
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    ---@type Gitsigns.Config
+    opts = {
+      signs = {
+        -- stylua: ignore start
+        add =          { text = "▎" },
+        change =       { text = "▎" },
+        delete =       { text = "▁" },
+        topdelete =    { text = "▔" },
+        changedelete = { text = "▎" },
+        untracked =    { text = "▎" },
+        -- stylua: ignore end
+      },
+      signs_staged = {
+        -- stylua: ignore start
+        add =          { text = "▎" },
+        change =       { text = "▎" },
+        delete =       { text = "▁" },
+        topdelete =    { text = "▔" },
+        changedelete = { text = "▎" },
+        -- stylua: ignore end
+      },
+      current_line_blame_formatter = "<author_time:%d/%m/%y %H:%MS> <author> - <summary>",
+      current_line_blame_opts = {
+        delay = 100,
+      },
+      on_attach = function(bufnr)
+        local gs = require("gitsigns")
+
+        vim.keymap.set("n", "]c", next_hunk, { buffer = bufnr, desc = "Next hunk" })
+        vim.keymap.set("n", "[c", previous_hunk, { buffer = bufnr, desc = "Previous hunk" })
+        vim.keymap.set("n", "<leader>tb", gs.toggle_current_line_blame, { buffer = bufnr, desc = "[T]oggle line [B]lame" })
+        vim.keymap.set("n", "<leader>td", gs.toggle_deleted, { buffer = bufnr, desc = "[T]oggle [D]eleted" })
+        vim.keymap.set("n", "<leader>gd", gs.diffthis, { buffer = bufnr, desc = "[G]it [D]iff" })
+        vim.keymap.set("n", "<leader>gp", gs.preview_hunk_inline, { buffer = bufnr, desc = "[G]it [P]review hunk" })
+        vim.keymap.set("n", "<leader>gs", gs.stage_hunk, { buffer = bufnr, desc = "[G]it toggle [S]tage hunk" })
+        vim.keymap.set("v", "<leader>gs", stage_visual_hunk, { buffer = bufnr, desc = "[G]it toggle [S]tage hunk" })
+      end,
+    },
   },
 }
