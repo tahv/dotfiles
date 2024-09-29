17628138f02792a7424361a6a5b1ccca2e692e6b
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-09-29 22:33:44 +0200

    moved window keymaps to keymap file

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -1,56 +1,83 @@
-vim.keymap.set('n', '<leader>bb', "<cmd>e #<cr>", { desc = 'Switch to other [B]uffer' })
+vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to other [B]uffer" })
 -- vim.keymap.set('n', '<leader>e', ':Explore<cr>', { desc = 'Open [E]xplorer' })
-
 -- Clear search with <esc>
 vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
 -- Move by screen line if no count was given, otherwise move by text line
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Manage windows (tmux split keys)
-vim.keymap.set('n', '<leader>w"', '<C-W>s', { desc = 'Split [W]indow below' })
-vim.keymap.set('n', '<leader>w%', '<C-w>v', { desc = 'Split [W]indow right' })
-vim.keymap.set('n', '<leader>wd', '<C-w>c', { desc = '[W]indow [D]elete' })
+vim.keymap.set("n", '<leader>w"', "<C-W>s", { desc = "Split [W]indow below" })
+vim.keymap.set("n", "<leader>w%", "<C-w>v", { desc = "Split [W]indow right" })
+vim.keymap.set("n", "<leader>wd", "<C-w>c", { desc = "[W]indow [D]elete" })
 
 -- Navigate windows using <ctrl> hjkl keys
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to left window' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to upper window' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to right window' })
-
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 
 -- Resize window using <ctrl> arrow keys
-local window = require("tahv.window")
-vim.keymap.set("n", "<C-Up>", window.resize_up, { desc = "Resize window up" })
-vim.keymap.set("n", "<C-Down>", window.resize_down, { desc = "Resize Window down" })
-vim.keymap.set("n", "<C-Left>", window.resize_left, { desc = "Resize window left" })
-vim.keymap.set("n", "<C-Right>", window.resize_right, { desc = "Resize window right" })
+
+vim.keymap.set("n", "<C-Up>", function()
+  if vim.fn.winnr() == vim.fn.winnr("j") then -- at bottom edge
+    vim.api.nvim_win_set_height(0, vim.api.nvim_win_get_height(0) + 2)
+  else
+    vim.api.nvim_win_set_height(0, vim.api.nvim_win_get_height(0) - 2)
+  end
+end, { desc = "Resize window up" })
+
+vim.keymap.set("n", "<C-Down>", function()
+  if vim.fn.winnr() == vim.fn.winnr("j") then -- at bottom edge
+    if vim.fn.winnr() ~= vim.fn.winnr("k") then -- not at top edge
+      vim.api.nvim_win_set_height(0, vim.api.nvim_win_get_height(0) - 2)
+    end
+  else
+    vim.api.nvim_win_set_height(0, vim.api.nvim_win_get_height(0) + 2)
+  end
+end, { desc = "Resize Window down" })
+
+vim.keymap.set("n", "<C-Left>", function()
+  if vim.fn.winnr() == vim.fn.winnr("l") then -- at right edge
+    vim.api.nvim_win_set_width(0, vim.api.nvim_win_get_width(0) + 2)
+  else
+    vim.api.nvim_win_set_width(0, vim.api.nvim_win_get_width(0) - 2)
+  end
+end, { desc = "Resize window left" })
+
+vim.keymap.set("n", "<C-Right>", function() -- at right edge
+  if vim.fn.winnr() == vim.fn.winnr("l") then
+    vim.api.nvim_win_set_width(0, vim.api.nvim_win_get_width(0) - 2)
+  else
+    vim.api.nvim_win_set_width(0, vim.api.nvim_win_get_width(0) + 2)
+  end
+end, { desc = "Resize window right" })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 
 -- Center buffer when moving
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move half screen up' })
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move half screen down' })
-vim.keymap.set('n', 'gg', 'ggzz', { desc = 'Go to the first line of the document' })
-vim.keymap.set('n', 'G', 'Gzz', { desc = 'Go to the last line of the document' })
-vim.keymap.set('n', 'n', 'nzz', { desc = 'Repeat search in same direction' })
-vim.keymap.set('n', 'N', 'Nzz', { desc = 'Repeat search in opposite direction' })
-vim.keymap.set('n', '<C-o>', '<C-o>zz', { desc = 'Go to older position in jump list' })
-vim.keymap.set('n', '<C-i>', '<C-i>zz', { desc = 'Go to newer position in jump list' })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move half screen up" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move half screen down" })
+vim.keymap.set("n", "gg", "ggzz", { desc = "Go to the first line of the document" })
+vim.keymap.set("n", "G", "Gzz", { desc = "Go to the last line of the document" })
+vim.keymap.set("n", "n", "nzz", { desc = "Repeat search in same direction" })
+vim.keymap.set("n", "N", "Nzz", { desc = "Repeat search in opposite direction" })
+vim.keymap.set("n", "<C-o>", "<C-o>zz", { desc = "Go to older position in jump list" })
+vim.keymap.set("n", "<C-i>", "<C-i>zz", { desc = "Go to newer position in jump list" })
 
--- Don't lose selection when shifting sidewards
+-- Keep selection when shifting sidewards
 -- https://github.com/mhinz/vim-galore#dont-lose-selection-when-shifting-sidewards
-vim.keymap.set("v", "<", "<gv", { desc = 'Dedent' })
-vim.keymap.set("v", ">", ">gv", { desc = 'Indent' })
+vim.keymap.set("v", "<", "<gv", { desc = "Dedent" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent" })
 
 -- The direction of n and N depends on whether / (forward) or ? (backward) was used.
 -- Remapping 'n' to always search forward and 'N' backward.
