90e0bc1e2aabc7856c5e9dd34052028c96800afd
Thibaud Gambier <thibaud.gambier@gmail.com>
2024-10-28 22:33:36 +0100

    updated autogroup name

--- neovim/nvim/lua/config/autocmds.lua
+++ neovim/nvim/lua/config/autocmds.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight on yank",
-  group = vim.api.nvim_create_augroup("tahv-highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
