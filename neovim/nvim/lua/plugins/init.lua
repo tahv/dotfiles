1d3d2495d6644135182885dbf3decf297a050b64
Thibaud Gambier <thibaud.gambier@gmail.com>
2025-01-26 10:35:07 +0100

    Add hardtime.nvim

--- neovim/nvim/lua/plugins/init.lua
+++ neovim/nvim/lua/plugins/init.lua
@@ -216,4 +216,10 @@ return {
     event = "LspAttach",
     opts = {},
   },
+  {
+    "m4xshen/hardtime.nvim",
+    enabled = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+  },
 }
