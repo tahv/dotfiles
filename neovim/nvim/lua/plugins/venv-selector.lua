581fa5c9c184a0d9dcb0230e48c6438b34b41faa
Thibaud Gambier <thibaud.gambier@gmail.com>
2025-12-19 16:34:08 +0100

    Add venv-selector

---
+++ neovim/nvim/lua/plugins/venv-selector.lua
@@ -0,0 +1,22 @@
+---@type LazySpec[]
+return {
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      -- "neovim/nvim-lspconfig",
+      { "folke/snacks.nvim" },
+    },
+    ft = "python",
+    keys = {
+      { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "select [v]env" },
+    },
+    opts = {
+      ---@type venv-selector.Options
+      options = {
+        picker = "snacks",
+        debug = true,
+        notify_user_on_venv_activation = true,
+      },
+    },
+  },
+}
