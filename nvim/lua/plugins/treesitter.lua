f90219a0a94afb80632c316faffa8e03cac9be72
Thibaud Gambier <account+gitlab@tgambier.fr>
2026-08-09 20:13:56 +0200

    feat(nvim): add treesitter injection for mise tasks
    Add treesitter injection for `task.run` in mise.toml files

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -127,6 +127,12 @@ local function swap_prev(query_string, query_group)
   end
 end
 
+require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+  local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+  local filename = vim.fn.fnamemodify(filepath, ":t")
+  return string.match(filename, ".*mise.*%.toml$") ~= nil
+end, { force = true, all = false })
+
 ---@type LazySpec[]
 return {
   {
