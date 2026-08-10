307dff343286fa2a133b43ab4b47716621f94f92
Thibaud Gambier <account+gitlab@tgambier.fr>
2026-08-10 20:22:35 +0200

    feat(clink): remove unused clink module

--- clink/scripts/init.lua
+++ clink/scripts/init.lua
@@ -10,19 +10,3 @@ alias("cdp", "cd /D D:\\p4ws\\$1")
 alias("ls", "dir /B $*")
 alias("clear", "cls")
 alias("rgf", "rg --files ^| rg $*")
-
----@param line string
----@return string | string[] | nil
----@return boolean | nil
-local function command_cdd(line)
-    local prefix = "^ *cdd *"
-	if line:match(prefix) then
-        line = line:gsub(prefix, "")
-	else
-		return
-	end
-    line = "cd /D D:\\dev\\" .. line
-    return line, false
-end
-
-clink.onfilterinput(command_cdd)
