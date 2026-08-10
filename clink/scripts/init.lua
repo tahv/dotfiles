4cd251d7622654e09967515f2bac638d13a50152
Thibaud Gambier <account+gitlab@tgambier.fr>
2026-08-10 20:24:17 +0200

    feat(clink): add aliases

--- clink/scripts/init.lua
+++ clink/scripts/init.lua
@@ -5,8 +5,11 @@ local function alias(short, long)
 	os.execute("doskey " .. short .. "=" .. long)
 end
 
--- alias("cdd", "cd /D D:\\dev\\$1")
+alias("cdd", "cd /D D:\\dev\\$1")
 alias("cdp", "cd /D D:\\p4ws\\$1")
 alias("ls", "dir /B $*")
 alias("clear", "cls")
 alias("rgf", "rg --files ^| rg $*")
+alias("which", "where $*")
+alias("pp", '@for %g in ("%PATH:;=" "%")Do @Echo(%~g')
+alias("mr", "mise run $*")
