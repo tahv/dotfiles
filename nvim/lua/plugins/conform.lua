fdd8b0758373a2aca53f9702b7c2f08ef8cf1c36
Thibaud Gambier <account+gitlab@tgambier.fr>
2026-08-12 22:53:43 +0200

    feat(nvim): format nu files

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -48,6 +48,7 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         rust = { "rustfmt", lsp_format = "fallback" },
+        nu = { "nufmt", lsp_format = "fallback" },
         toml = { "tombi" },
         yaml = { lsp_format = "first", "trim_newlines", "trim_whitespace" },
         markdown = { "rumdl", "trim_newlines", "trim_whitespace" },
