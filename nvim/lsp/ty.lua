9cf9221ac60afabb0390a13d28f859dca652495d
Thibaud Gambier <account+gitlab@tgambier.fr>
2026-09-18 17:35:01 +0200

    feat(nvim): enable experimental `useUv` feature
    - Requires uv 0.12.3 or later
    - Use uv to create and update environments for PEP 723 scripts
    - See
    <https://docs.astral.sh/ty/reference/editor-settings/#experimentaluseuv>

--- nvim/lsp/ty.lua
+++ nvim/lsp/ty.lua
@@ -1,6 +1,11 @@
 -- https://docs.astral.sh/ty/reference/editor-settings/
 ---@type vim.lsp.Config
 return {
+  init_options = {
+    experimental = {
+      useUv = "scripts",
+    },
+  },
   settings = {
     ty = {
       diagnosticMode = "workspace",
